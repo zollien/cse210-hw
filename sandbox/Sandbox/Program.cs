8792c7621cda93cc260898795b9843150de8920c
zollien <dov19003@byui.edu>
2024-11-03 21:23:51 +0000

    added message like video

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox New World!!!!!!");
+        Console.WriteLine("Hello Sandbox World!!!");
     }
 }
