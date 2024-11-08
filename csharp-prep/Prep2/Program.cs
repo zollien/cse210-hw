f6b04298c612ad2a67adb9d47116eb13aa818da8
zollien <dov19003@byui.edu>
2024-11-08 20:32:00 +0000

    Prep2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,10 +1,47 @@
 using System;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage grade? ");
+        string grade_input = Console.ReadLine();
+        int percentage_grade = int.Parse(grade_input);
+
+        string grade = "";
+
+        if (percentage_grade >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage_grade >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage_grade >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage_grade >= 60)
+        {
+            grade = "D";
+        }
+        else if (percentage_grade < 60)
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is: {grade}.");
+        if (percentage_grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Sign up for tutoring!");
+        }
+
     }
 }
