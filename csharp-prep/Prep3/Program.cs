74e7a68616fd4e684f96cc700a83340ca89b4cac
zollien <dov19003@byui.edu>
2024-11-08 23:25:51 +0000

    Prep3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Prep3 Loops
+
+        // call random number generator
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+        //Range for random number 1 to 11 if (1, 11)
+        Console.WriteLine("Can you guess the magic number? ");
+
+        int guess_input = -1;
+
+        while (guess_input != magic_number)
+        {
+            //use
+            Console.Write("What is the magic number? ");
+            guess_input = int.Parse(Console.ReadLine());
+
+            if (guess_input > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess_input < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess_input == magic_number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            // break; can it be used here to stop the loop?
+
+        }
     }
-}
