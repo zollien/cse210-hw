31a80609e72faeb4a04dc836dece3e388ac91642
zollien <dov19003@byui.edu>
2024-11-18 22:21:14 +0000

    w1 prep5 with issues

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,25 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        static void Welcome()
-        {
-            Console.WriteLine("Welcome to the program!");
-        }
-        
-        static void Name(string name)
-        {
-            Console.WriteLine("Please enter your name: ");
-            string name = Console.ReadLine();
-        }
+        // is this main, is this like calling in the main in python where you have to identify each function that is about to be used in the program?
+        DisplayWelcome();
+        string name = PromtUserName();
+        int favnumber = PromptUserNumber();
+        int square = SquareNumber(favnumber);
+        DisplayResult(name, square);
+    }
 
-        static void SquareNumber(int favnumber)
-        {
-            Console.WriteLine("Please enter your favorite number: ");
-            int favnumber = Console.ReadLine();
+    //Displays the message, "Welcome to the Program!"
+    DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    /*can use static voice in function name if you want ti to return something AND function names should have empty(), unles asking to compute something*/
+    //Asks for and returns the user's name (as a string)
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
 
-            int square = Math.Sqrt(favnumber);
-        }
+    //Asks for and returns the user's favorite number (as an integer)
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int favnumber = int.Parse(Console.ReadLine());
+        return favnumber;
+    }
 
+    //Accepts an integer as a parameter and returns that number squared (as an integer) - int needed in () to enable return int
+    static int SquareNumber(int favnumber)
+    {
+        int square = favnumber * favnumber;
+        return square;
+    }
+
+    //Accepts the user's name and the squared number and displays them.
+    static void DisplayResult(string name, int square)
+    {   
         Console.WriteLine($"{name}, the square of your number is {square}.");
     }
 }
