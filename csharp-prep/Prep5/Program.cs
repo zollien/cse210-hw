86d6c848420148d2d69b3a52e3d8e1d950844fb8
zollien <dov19003@byui.edu>
2024-11-18 21:51:00 +0000

    w1 effort

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        
+        static void Name(string name)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+        }
+
+        static void SquareNumber(int favnumber)
+        {
+            Console.WriteLine("Please enter your favorite number: ");
+            int favnumber = Console.ReadLine();
+
+            int square = Math.Sqrt(favnumber);
+        }
+
+        Console.WriteLine($"{name}, the square of your number is {square}.");
     }
 }
