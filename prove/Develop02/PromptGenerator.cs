39c62a47d04bef372b42a0f9d166c34b7db30a6d
zollien <dov19003@byui.edu>
2024-11-18 18:07:00 +0000

    prep4 complete

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -5,5 +5,5 @@ public List<string> _prompts;
 public string GetRandomPrompt()
 {
     return "";
-    //this returns an empty string and builds without error because we asked it to return something, even though it was an empty string.
+    /* this returns an empty string and builds without error because we asked it to return something, even though it was an empty string. */
 }
