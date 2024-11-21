bdf3a4e3d7febefc52e39f8dce968f6f6cb6a30f
zollien <dov19003@byui.edu>
2024-11-21 21:15:07 +0000

    w2 prove1

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,10 +1,45 @@
+using System.IO;
 public class Journal
 {
+    //class3 to write - 50% ? INCOMPLETE
     public List<Entry> _entries;
 
-    public void AddEntry(Entry newEntry)
+    public void AddEntry() //paremeters are not needed inside AddEntry(Entry entry)
     {
+        Entry entry = new Entry(); //this is a constructor covered in w04 info
 
+
+        //entry date added from DateTime.Now in Journal
+        entry _date
+        {
+            //code from assignment
+            DateTime theCurrentTime = DateTime.Now;
+            string dateText = theCurrentTime.ToShortDateString()
+        }
+
+        //prompt text added fro PromptGenerator
+        entry _promptText
+        {
+
+        }
+
+        //entry text added from User input
+        entry _entryText
+        {
+            //code from assignment
+            string fileName = "myFile.txt";
+
+            using (StreamWriter outputFile = new StreamWriter(filename))
+            {
+                // You can add text to the file with the WriteLine method
+                outputFile.WriteLine("This will be the first line in the file.");
+
+                // You can use the $ and include variables just like with Console.WriteLine
+                string color = "Blue";
+                outputFile.WriteLine($"My favorite color is {color}");
+                //this looks like it prints to an outputFile to write the line
+            }
+        }
     }
     public void DisplayAll()
     {
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,9 +1,25 @@
+//PromptGenerator - complete
 public class PromptGenerator
-
-public List<string> _prompts;
-
-public string GetRandomPrompt()
 {
-    return "";
-    /* this returns an empty string and builds without error because we asked it to return something, even though it was an empty string. */
+    //class 1 to write
+    public List<string> _prompts = new List<string>()
+    {
+        "Which scripture inspired you today?",
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };   //;after the bracket because it represents the body of a data structure/list that we are declaring
+
+    // call random number generator
+    Random randomGenerator = new Random();
+    public string GetRandomPrompt()
+    {
+        int i = randomGenerator.Next(_prompts.Count);
+        // int i - i is the index number   - _prompt.Count allows the random selections from with the available i's - .Count can be applied to lists to count the entries and keeps the random index with the range of i available.
+        return _prompts[i]; //_prompts[i] means at position i in the index
+
+        /* this returns an empty string and builds without error because we asked it to return something, even though it was an empty string. */
+    }
 }
