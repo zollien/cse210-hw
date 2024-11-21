b53e4bb46abf1fd64ad3630de85624c74831afd8
zollien <dov19003@byui.edu>
2024-11-21 21:13:58 +0000

    w3prove1

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,12 @@
 using System;
 
 class Program
+//class4 to write last - Unstarted
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+
     }
 }
