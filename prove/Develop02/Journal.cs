549acf7d579462412da771878def7e3788cdba78
zollien <dov19003@byui.edu>
2024-11-29 21:07:50 +0000

    w2 prove

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -2,57 +2,56 @@ using System.IO;
 public class Journal
 {
     //class3 to write - 50% ? INCOMPLETE
-    public List<Entry> _entries;
+    public List<Entry> _entries = new List<Entry>(); //calls the constructor of the class and initialises it
+
+    public PromptGenerator _promptgen = new PromptGenerator();
 
     public void AddEntry() //paremeters are not needed inside AddEntry(Entry entry)
     {
         Entry entry = new Entry(); //this is a constructor covered in w04 info
-        entry._date = DateOnly
-        _entries
+        entry._date = DateTime.Now.ToShortDateString();
 
-        //entry date added from DateTime.Now in Journal
-        entry _date
-        {
-            //code from assignment
-            DateTime theCurrentTime = DateTime.Now;
-            string dateText = theCurrentTime.ToShortDateString()
-        }
-
-        //prompt text added fro PromptGenerator
-        entry _promptText
-        {
-
-        }
-
+        //prompt text added from PromptGenerator
+        entry._promptText = _promptgen.GetRandomPrompt();  //to execute prompt gen
+        Console.WriteLine($"{entry._promptText}");
         //entry text added from User input
-        entry _entryText
-        {
-            //code from assignment
-            string fileName = "myFile.txt";
-
-            using (StreamWriter outputFile = new StreamWriter(filename))
-            {
-                // You can add text to the file with the WriteLine method
-                outputFile.WriteLine("This will be the first line in the file.");
-
-                // You can use the $ and include variables just like with Console.WriteLine
-                string color = "Blue";
-                outputFile.WriteLine($"My favorite color is {color}");
-                //this looks like it prints to an outputFile to write the line
-            }
-        }
+        entry._entryText = Console.ReadLine();
+        _entries.Add(entry);
     }
     public void DisplayAll()
     {
+        foreach (Entry entry in _entries)
+        {
+            Console.WriteLine(entry.Display());
 
+        }
     }
     public void SaveToFile(string file)
     {
+        //code from assignment
 
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}\n");
+            }
+        }
     }
     public void LoadFromFile(string file)
     {
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(file);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._entryText = parts[2];
+            _entries.Add(entry);
 
+        }
     }
 
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,13 +1,52 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 //class4 to write last - Unstarted
+
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        while (true)
+        {
+            Console.WriteLine($"Menu\n "
+            + "1 write \n"
+            + "2 display\n"
+            + "3 save \n"
+            + "4 load \n"
+            + "5 quit");
 
-        
+            int choice = int.Parse(Console.ReadLine());
 
+            if (choice == 1)
+            {
+                journal.AddEntry();
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Enter the file name");
+                string file = Console.ReadLine();
+                journal.SaveToFile(file);
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Enter the file name");
+                string file = Console.ReadLine();
+                journal.LoadFromFile(file);
+            }
+            else if (choice == 5)
+            {
+                break; //quits
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
     }
 }
