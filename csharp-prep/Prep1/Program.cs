4f19b9d8b8d3ce38e2c527ccc177dbb03a424961
zollien <dov19003@byui.edu>
2025-01-11 19:54:50 +0000

    W01 00 prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
-
-class Program
+/*using system call the system name space*/
+/*"namepaces game" the there is ususally a namesspace which like a package that can be referenced game would be the name of the package */
+class Program /*the class program is the shell that contains teh program*/
 {
-    static void Main(string[] args)
+    static void Main(string[] args) /*here we call the main program, static means , void means this program will return nothing, (string[] args says the program can recieve strings which are arguements*/
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /*code that will run is placed in {} inside the main*/
+        Console.WriteLine("This is Zollien Dove's C# Code.");
+
+        /*Program code*/
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}. ");
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,13 @@
 using System;
-
-class Program
+/*using system call teh system name space*/
+/*"namepaces game" the there is ususally a namesspace which like a package that can be referenced game would be the name of the package */
+class Program /*the class program is the shell that contains teh program*/
 {
-    static void Main(string[] args)
+    static void Main(string[] args) /*here we call the main program, static means , void means this program will return nothing, (string[] args says the program can recieve strings which are arguements*/
     {
+        /*code that will run is placed in {} inside the main*/
         Console.WriteLine("Hello Learning02 World!");
+        /*Program code*/
+
     }
 }
