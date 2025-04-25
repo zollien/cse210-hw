a0252b73c6cf0869bb0f6281c8036d52f3ba5f93
Zollien <dov19003@byui.edu>
2025-04-25 09:43:10 +0100

    W01 exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        /* without the $ the program will just put what is written, with the $ it will select the variables in {} */
+
     }
 }
